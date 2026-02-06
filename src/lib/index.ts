7440146e7da0c9e4cd104f2476d05da2749c3428
Edwin Zhao <contact@edwinzhao.com>
2026-02-06 13:23:24 -0800

    fix: ajdusting test for comment

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,56 +4,63 @@
  * A reusable timeline component library for Svelte 5 with canvas rendering.
  */
 
-// Core types
+// Core typess
 export type {
-	DragTarget,
-	HitTarget,
-	TimelineState,
-	RenderContext,
-	RenderLayer
-} from './core/types';
+  DragTarget,
+  HitTarget,
+  TimelineState,
+  RenderContext,
+  RenderLayer,
+} from "./core/types";
 
 // Utilities
 export {
-	clamp,
-	mapRange,
-	formatTime,
-	calculateGridInterval,
-	generateGridLines,
-	zoomAtPoint,
-	panView,
-	getDevicePixelRatio,
-	resetShadow
-} from './core/utils';
+  clamp,
+  mapRange,
+  formatTime,
+  calculateGridInterval,
+  generateGridLines,
+  zoomAtPoint,
+  panView,
+  getDevicePixelRatio,
+  resetShadow,
+} from "./core/utils";
 
 // Store
-export { createTimelineStore, type TimelineStore, type TimelineStoreConfig } from './core/store.svelte';
+export {
+  createTimelineStore,
+  type TimelineStore,
+  type TimelineStoreConfig,
+} from "./core/store.svelte";
 
 // Config
 export {
-	createColorScheme,
-	defaultColorScheme,
-	type TimelineColorScheme
-} from './config/colors';
+  createColorScheme,
+  defaultColorScheme,
+  type TimelineColorScheme,
+} from "./config/colors";
 
 export {
-	createLayoutConfig,
-	defaultLayoutConfig,
-	type TimelineLayoutConfig
-} from './config/layout';
+  createLayoutConfig,
+  defaultLayoutConfig,
+  type TimelineLayoutConfig,
+} from "./config/layout";
 
 // Rendering layers
 export {
-	BackgroundLayer,
-	PlayheadLayer,
-	HoverLayer,
-	ZoomSelectionLayer
-} from './rendering/layers/index';
+  BackgroundLayer,
+  PlayheadLayer,
+  HoverLayer,
+  ZoomSelectionLayer,
+} from "./rendering/layers/index";
 
 // Renderer
-export { TimelineRenderer, type TimelineRendererConfig } from './rendering/renderer';
+export {
+  TimelineRenderer,
+  type TimelineRendererConfig,
+} from "./rendering/renderer";
 
 // Components
-export { default as Timeline } from './components/Timeline.svelte';
-export { default as TimelineCanvas } from './components/TimelineCanvas.svelte';
-export { default as TimelineControls } from './components/TimelineControls.svelte';
+export { default as Timeline } from "./components/Timeline.svelte";
+export { default as TimelineCanvas } from "./components/TimelineCanvas.svelte";
+export { default as TimelineControls } from "./components/TimelineControls.svelte";
